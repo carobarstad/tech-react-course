7fc1cb3b182ebb28d2cb1126b23461afcb70f4dd
Mimi Nguyen <mimin@netcompany.com>
2020-10-16 19:01:08 +0200

    Added comments for CharacterDetails

--- src/components/CharacterDetails.tsx
+++ src/components/CharacterDetails.tsx
@@ -5,8 +5,13 @@ import { ICharacter } from '../interface/ICharactersResponse';
 import './character.css';
 
 const CharacterDetails = ({ name, height, birth_year, url }: ICharacter) => {
+  // A part of the character url
   const people = 'people/';
+
+  // Figure out what index people start at
   const peopleIndex = url.indexOf(people);
+
+  // Split the character url and remove the last /
   const id = url
     .substring(peopleIndex + people.length, url.length)
     .replace(/\//g, '');
