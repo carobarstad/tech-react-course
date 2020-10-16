29225f3383867f372db9127804dd3e1fcdcba9fe
Mimi Nguyen <mimin@netcompany.com>
2020-10-16 18:47:24 +0200

    Added logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Router from './router/Router';
 function App() {
   return (
     <div className="App">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/640px-Star_Wars_Logo.svg.png"
+        alt="star wars logo"
+      />
       <Router />
     </div>
   );
